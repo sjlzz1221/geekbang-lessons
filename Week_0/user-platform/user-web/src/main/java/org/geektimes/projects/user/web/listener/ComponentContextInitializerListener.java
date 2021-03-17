000ebc667448f4a294112bdcab9b11c8b417dfd2
sjlzz1221 <1633358664@qq.com>
2021-03-17 18:06:57 +0800

    第三周作业

--- Week_0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ Week_0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -1,10 +1,14 @@
 package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.context.ComponentContext;
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.management.UserManager;
 
+import javax.management.*;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 
 /**
  * {@link ComponentContext} 初始化器
@@ -19,6 +23,18 @@ public class ComponentContextInitializerListener implements ServletContextListen
         this.servletContext = sce.getServletContext();
         ComponentContext context = new ComponentContext();
         context.init(servletContext);
+
+        // jolokia
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName objectName = null;
+        // 创建 UserMBean 实例
+        User user = new User();
+        try {
+            objectName = new ObjectName("jolokia:name=User");
+            mBeanServer.registerMBean(new UserManager(user), objectName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
